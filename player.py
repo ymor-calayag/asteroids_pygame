bf4f7167e9a13f49b3ed2899686020b2e1c49bbf
ymor-calayag <jeffreyymor.calayag@gmail.com>
2025-10-04 13:58:25 +0800

    Add player rotation with A/D keys

--- player.py
+++ player.py
@@ -18,3 +18,14 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
