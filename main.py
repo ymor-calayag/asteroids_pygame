fa0584a12f23e0c8801898ff227c537e9ffc368d
ymor-calayag <jeffreyymor.calayag@gmail.com>
2025-10-06 13:37:11 +0800

    feat(asteroid): add split() to spawn two faster smaller asteroids

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
             for shot in shots:
                 if asteroid.collides_with(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
