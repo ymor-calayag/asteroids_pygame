691f474f6b592294db0cf710334fcdd0bc204ae4
ymor-calayag <jeffreyymor.calayag@gmail.com>
2025-10-06 13:18:50 +0800

    refactor(main): replace direct player calls with group-based update/draw

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
